709f1dd4c944a273c0a6ef18e8e2774e304d2b08
Yassine Hamdouni <67022362+yassinehamdouni@users.noreply.github.com>
2020-09-10 17:00:56 -0700

    Update master_xml_workable.py

--- workable:api/master_xml_workable.py
+++ workable:api/master_xml_workable.py
@@ -20,7 +20,7 @@ class handler(BaseHTTPRequestHandler):
                     titleXml = ET.SubElement(jobXml, 'title')
                     titleXml.text = job["title"]
                     employerXml = ET.SubElement(jobXml, 'Email')
-                    employerXml.text = f"team+{all_companies[i]}@climate.careers"
+                    employerXml.text = f"team+{all_companies[i].split()[0]}@climate.careers"
                     urlXml = ET.SubElement(jobXml, 'url')
                     urlXml.text = job["url"]
                     locationXml = ET.SubElement(jobXml, 'location')
